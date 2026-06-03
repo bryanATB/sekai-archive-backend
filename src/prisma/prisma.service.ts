a1abead18c678be0ce06fc71e2783d7f62fd2460
bryanATB <bryanbasado16@gmail.com>
2026-06-03 00:58:31 -0500

    release

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,6 +10,7 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
+    console.log('DATABASE_URL:', process.env.DATABASE_URL?.substring(0, 50));
     if (process.env.NODE_ENV === 'production') {
       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
     }
